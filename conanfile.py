320c5804dfe9b7b5e8b01a75a2c5c75e125b3e1c
dimitri <dimi309@users.noreply.github.com>
2016-06-11 23:20:19 +0200

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,51 @@
+from conans import ConanFile, CMake
+import os
+
+class GlewConan(ConanFile):
+    name = "glew"
+    version = "1.13.0"
+    ZIP_FOLDER_NAME = "%s-%s" % (name, version)
+    generators = "cmake"
+    settings = "os", "arch", "build_type", "compiler"
+    #options = {"shared": [True, False], "fPIC": [True, False]}
+    #default_options = "shared=False", "fPIC=False"
+    url="http://github.com/coging3d/conan-glew"
+    requires = ""
+    license="https://github.com/nigels-com/glew#copyright-and-licensing"
+
+    def config(self):
+        try: # Try catch can be removed when conan 0.8 is released
+            del self.settings.compiler.libcxx
+        except:
+            pass
+
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run("cd %s && mkdir _build" % self.ZIP_FOLDER_NAME)
+        cd_build = "cd %s/_build" % self.ZIP_FOLDER_NAME
+        self.run('%s && cmake ../build/cmake %s' % (cd_build, cmake.command_line))
+        self.run("%s && cmake --build . %s" % (cd_build, cmake.build_config))
+
+    def package(self):
+        # Copying headers
+        self.copy("*.h", "include", "%s" % (self.ZIP_FOLDER_NAME), keep_path=False)
+
+        # Copying static and dynamic libs
+        if self.settings.os == "Windows":
+            if self.options.shared:
+                self.copy(pattern="*.dll", dst="bin", src=self.ZIP_FOLDER_NAME, keep_path=False)
+            self.copy(pattern="*.lib", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+        else:
+            if self.options.shared:
+                if self.settings.os == "Macos":
+                    self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+                else:
+                    self.copy(pattern="*.so*", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+            else:
+                self.copy(pattern="*.a", dst="lib", src=self.ZIP_FOLDER_NAME, keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ['libGLEW']
