06306828ebb4783323fe1d0f7b3a47b8d4bf25d2
dimitri <dimi309@users.noreply.github.com>
2017-04-22 08:52:25 +0200

    vorbis: build missing requirements

--- vorbis/build.py
+++ vorbis/build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 import os, platform
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(args="--build missing")
     builder.add_common_builds()
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
