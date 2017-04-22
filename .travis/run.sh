837630da18e993b4fcb00f85c71498dcf06d9b4d
dimitri <dimi309@users.noreply.github.com>
2017-04-22 21:01:59 +0200

    Building all packages

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,11 +10,11 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-: 'cd ogg
+cd ogg
 python build.py
 cd ..
 cd vorbis
 python build.py
-cd ..'
+cd ..
 cd glew
 python build.py
