bebb1589385bdeaae085fda86c4c9a3fbbdcf585
dimitri <dimi309@users.noreply.github.com>
2017-04-23 10:48:22 +0200

    Added dependency scanning for Debian

--- glfw/conanfile.py
+++ glfw/conanfile.py
@@ -1,5 +1,6 @@
-from conans import ConanFile, CMake, os
+from conans import ConanFile, CMake
 from conans.tools import download, unzip
+import os, subprocess
 
 class GlfwConan(ConanFile):
     name = "glfw"
@@ -12,19 +13,56 @@ class GlfwConan(ConanFile):
     license="https://github.com/glfw/glfw/blob/master/LICENSE.md"
     exports = "*"
 
+    def rpm_package_installed(self, package):
+        p = subprocess.Popen(['rpm', '-q', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, _ = p.communicate()
+        return 'install ok' in out or 'not installed' not in out
+
+    def ensure_rpm_dependency(self, package):
+        if not self.rpm_package_installed(package):
+            self.output.warn(package + " is not installed in this machine! Conan will try to install it.")
+            # Note: yum is automatically redirected to dnf on modern Fedora distros (see 'man yum2dnf')
+            self.run("sudo yum install -y " + package)
+            if not self.rpm_package_installed(package):
+                self.output.error(package + " Installation doesn't work... install it manually and try again")
+                exit(1)
+
+    def debian_package_installed(self, package):
+        p = subprocess.Popen(['dpkg', '-s', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, _ = p.communicate()
+        return 'install ok' in out
+
+    def ensure_debian_dependency(self, package):
+        if not self.debian_package_installed(package):
+            self.output.warn(package + " is not installed in this machine! Conan will try to install it.")
+            self.run("sudo apt-get update && sudo apt-get install -y " + package)
+            if not self.debian_package_installed(package):
+                self.output.error(package + " Installation doesn't work... install it manually and try again")
+                exit(1)
+
+    def system_requirements(self):
+        if subprocess.call("which apt-get", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
+            self.ensure_debian_dependency("libglu1-mesa-dev")
+            self.ensure_debian_dependency("xorg-dev") 
+#        elif subprocess.call("which yum", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
+#            self.ensure_rpm_dependency("jack-audio-connection-kit-devel")
+#            self.ensure_rpm_dependency("alsa-lib-devel")
+        else:
+            self.output.warn("Could not determine Linux distro, skipping system requirements check.")
+
+
     def source(self):
         download("https://github.com/glfw/glfw/archive/%s.zip" % self.version, "%s.zip" % self.ZIP_FOLDER_NAME)
         unzip("%s.zip" % self.ZIP_FOLDER_NAME)
         os.unlink("%s.zip" % self.ZIP_FOLDER_NAME)
 
     def build(self):
-        
+        self.system_requirements()
         cmake = CMake(self)
         self.run("cmake %s/%s %s" % (self.conanfile_directory, self.ZIP_FOLDER_NAME, cmake.command_line))
         self.run("cmake --build %s %s" % (self.conanfile_directory, cmake.build_config))
 
     def package(self):
-
         self.copy("FindGLFW.cmake", ".", ".")
         
         self.copy(pattern="*.h", dst="include", src="%s/include" % self.ZIP_FOLDER_NAME, keep_path=True)
