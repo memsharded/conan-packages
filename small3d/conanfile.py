6e6c16cb99e12134a03a5c521f50971be8c49ec5
dimitri <dimi309@users.noreply.github.com>
2017-05-01 22:14:03 +0200

    Removed unused import

--- small3d/conanfile.py
+++ small3d/conanfile.py
@@ -1,6 +1,6 @@
 from conans import ConanFile, CMake
 from conans.tools import download, unzip, replace_in_file
-import os, subprocess
+import os
 
 class Small3dConan(ConanFile):
     name = "small3d"
