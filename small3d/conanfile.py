a96c5081d5c1aac521821bc4f58b646dca4af3d9
dimitri <dimi309@users.noreply.github.com>
2017-05-04 08:25:20 +0200

    Building with new portaudio package.

--- small3d/conanfile.py
+++ small3d/conanfile.py
@@ -12,7 +12,7 @@ class Small3dConan(ConanFile):
     url="http://github.com/dimi309/conan-packages"
     requires = "glfw/3.2.1@coding3d/stable", "freetype/2.6.3@lasote/stable","glew/2.0.0@coding3d/stable", \
         "libpng/1.6.23@lasote/stable","zlib/1.2.8@lasote/stable","glm/0.9.8.4@coding3d/stable", \
-        "vorbis/1.3.5@coding3d/stable", "portaudio/rc.v190600.20161001@coding3d/temp"
+        "vorbis/1.3.5@coding3d/stable", "portaudio/master@coding3d/temp"
     default_options = "glew:shared=False"
     license="https://github.com/dimi309/small3d/blob/master/LICENSE"
     exports = "FindSMALL3D.cmake"
