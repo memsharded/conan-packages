1b4d9be0946ea45ae93c3dac7786c9af7344f719
dimitri <dimi309@users.noreply.github.com>
2017-05-06 08:40:42 +0200

    Unit tests can now be built with conan, locally.

--- small3d/conanfile.py
+++ small3d/conanfile.py
@@ -9,11 +9,12 @@ class Small3dConan(ConanFile):
     ZIP_FOLDER_NAME = "%s-%s" % (name, version)
     generators = "cmake"
     settings = "os", "arch", "build_type", "compiler"
+    options = {"localUnitTests": [True, False]}
     url="http://github.com/dimi309/conan-packages"
     requires = "glfw/3.2.1@coding3d/stable", "freetype/2.6.3@lasote/stable","glew/2.0.0@coding3d/stable", \
         "libpng/1.6.23@lasote/stable","zlib/1.2.8@lasote/stable","glm/0.9.8.4@coding3d/stable", \
         "vorbis/1.3.5@coding3d/stable", "portaudio/rc.v190600.20161001@jgsogo/stable"
-    default_options = "glew:shared=False"
+    default_options = "glew:shared=False", "localUnitTests=False"
     license="https://github.com/dimi309/small3d/blob/master/LICENSE"
     exports = "FindSMALL3D.cmake"
 
@@ -21,9 +22,12 @@ class Small3dConan(ConanFile):
         download("https://github.com/dimi309/small3d/archive/%s.zip" % self.version, "%s.zip" % self.ZIP_FOLDER_NAME)
         unzip("%s.zip" % self.ZIP_FOLDER_NAME)
         os.unlink("%s.zip" % self.ZIP_FOLDER_NAME)
-        
+
+    def requirements(self):
+        if self.options.localUnitTests:
+            self.requires("gtest/1.8.0@lasote/stable")
+            
     def build(self):
-        self.system_requirements()
         replace_in_file("%s/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "project(small3d)",
                         """
 project(small3d)
@@ -45,18 +49,19 @@ file(COPY "small3d/include" DESTINATION ".")
 set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)
 endif()
 """)
-        
-        replace_in_file("%s/small3d/src/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "# Unit testing",
+        if not self.options.localUnitTests:
+            replace_in_file("%s/small3d/src/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "# Unit testing",
                         """
 if(FALSE)
                         """)
 
-        replace_in_file("%s/small3d/src/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "endif(APPLE)",
+            replace_in_file("%s/small3d/src/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "endif(APPLE)",
                         """
 endif(APPLE)
 endif()
                         """)
-        
+        else:
+            replace_in_file("%s/small3d/src/CMakeLists.txt" % self.ZIP_FOLDER_NAME, "target_link_libraries(small3dTest PUBLIC ${GTEST_BOTH_LIBRARIES} small3d )", "target_link_libraries(small3dTest PUBLIC ${CONAN_LIBS} small3d)")
         
         cmake = CMake(self)
         self.run("cmake %s/%s %s" % (self.conanfile_directory, self.ZIP_FOLDER_NAME, cmake.command_line))
