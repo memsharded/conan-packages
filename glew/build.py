8d26b11999976df67ceb6964cf67508c66208f20
dimitri <dimi309@users.noreply.github.com>
2017-05-29 04:31:45 +0200

    Added build.py script

---
+++ glew/build.py
@@ -0,0 +1,12 @@
+from conan.packager import ConanMultiPackager
+import os, platform
+
+if __name__ == "__main__":
+    builder = ConanMultiPackager()
+    builder.add_common_builds()
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if not (settings["compiler"] == "Visual Studio" and (settings["compiler.version"] != "15" or settings["arch"] == "x86")):
+            filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
+    builder.run()
