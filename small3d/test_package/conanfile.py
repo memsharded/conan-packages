6b15f35922db0f76b4ef9e678bbfc503f85ebf93
dimitri <dimi309@users.noreply.github.com>
2017-05-29 05:22:00 +0200

    Build missing packages when building small3d test.

--- small3d/test_package/conanfile.py
+++ small3d/test_package/conanfile.py
@@ -11,7 +11,7 @@ class TestGlew(ConanFile):
 
     def build(self):
         cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake "%s" %s --build missing' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def test(self):
