36dbbf6b1d135446f0f5c53f62b4bf4425b6e7ce
dimitri <dimi309@users.noreply.github.com>
2017-05-29 07:29:41 +0200

    When glfw is not built, small3d has to take care of the OpenGL dependency

--- small3d/conanfile.py
+++ small3d/conanfile.py
@@ -18,6 +18,41 @@ class Small3dConan(ConanFile):
     license="https://github.com/dimi309/small3d/blob/master/LICENSE"
     exports = ["FindSMALL3D.cmake"]
 
+    def rpm_package_installed(self, package):
+        p = subprocess.Popen(['rpm', '-q', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, _ = p.communicate()
+        return 'install ok' in out or 'not installed' not in out
+
+    def ensure_rpm_dependency(self, package):
+        if not self.rpm_package_installed(package):
+            self.output.warn(package + " is not installed in this machine! Conan will try to install it.")
+            # Note: yum is automatically redirected to dnf on modern Fedora distros (see 'man yum2dnf')
+            self.run("sudo yum install -y " + package)
+            if not self.rpm_package_installed(package):
+                self.output.error(package + " Installation doesn't work... install it manually and try again")
+                exit(1)
+
+    def debian_package_installed(self, package):
+        p = subprocess.Popen(['dpkg', '-s', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, _ = p.communicate()
+        return 'install ok' in out
+
+    def ensure_debian_dependency(self, package):
+        if not self.debian_package_installed(package):
+            self.output.warn(package + " is not installed in this machine! Conan will try to install it.")
+            self.run("sudo apt-get update && sudo apt-get install -y " + package)
+            if not self.debian_package_installed(package):
+                self.output.error(package + " Installation doesn't work... install it manually and try again")
+                exit(1)
+
+    def system_requirements(self):
+        if subprocess.call("which apt-get", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
+            self.ensure_debian_dependency("libglu1-mesa-dev")
+        elif subprocess.call("which yum", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
+            self.ensure_rpm_dependency("mesa-libGL-devel")
+        else:
+            self.output.warn("Could not determine package manager, skipping Linux system requirements installation.")
+    
     def source(self):        
         download("https://github.com/dimi309/small3d/archive/%s.zip" % self.version, "%s.zip" % self.ZIP_FOLDER_NAME)
         unzip("%s.zip" % self.ZIP_FOLDER_NAME)
