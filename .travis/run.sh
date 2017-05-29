1c1620efd7a90dc912887a85d90d83b3f0ca3807
dimitri <dimi309@users.noreply.github.com>
2017-05-29 05:40:57 +0200

    Prepared for travis ci.

--- .travis/run.sh
+++ .travis/run.sh
@@ -11,19 +11,25 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 cd glew
+export CONAN_REFERENCE=glew/2.0.0
 python build.py
 cd ..
 cd glfw
+export CONAN_REFERENCE=glfw/3.2.1
 python build.py
 cd ..
 cd glm
+export CONAN_REFERENCE=glm/0.9.8.4
 python build.py
 cd ..
 cd ogg
+export CONAN_REFERENCE=ogg/1.3.2
 python build.py
 cd ..
 cd vorbis
+export CONAN_REFERENCE=vorbis/1.3.5
 python build.py
 cd ..
 cd small3d
+export CONAN_REFERENCE=small3d/master
 python build.py
