3f206aeed6c53a78e208ef659dd8868214228bc1
dimitri <dimi309@users.noreply.github.com>
2017-05-31 21:29:41 +0200

    Limited glfw builds

--- glfw/build.py
+++ glfw/build.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     builder.add_common_builds()
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if not (settings["arch"] == "x86"):
+        if not (settings["arch"] == "x86" or (settings["compiler"] == "apple-clang" and (settings["compiler.version"] =="8.0"))):
             filtered_builds.append([settings, options, env_vars, build_requires])
+            print("%s %s %s %s " % (settings, options, env_vars, build_requires))
     builder.builds = filtered_builds
     builder.run()
