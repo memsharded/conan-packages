f35dd3216c91ab96c0521e3ce7915dd55388f48f
dimitri <dimi309@users.noreply.github.com>
2017-06-01 05:15:36 +0200

    ... and the opposite

--- glfw/build.py
+++ glfw/build.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     builder.add_common_builds()
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
-        if not (settings["arch"] == "x86" or (settings["compiler"] == "apple-clang" and (settings["build_type"] =="Debug"))):
+        if not (settings["arch"] == "x86" or (settings["compiler"] == "apple-clang" and (settings["build_type"] =="Release"))):
             filtered_builds.append([settings, options, env_vars, build_requires])
             print("%s %s %s %s " % (settings, options, env_vars, build_requires))
     builder.builds = filtered_builds
